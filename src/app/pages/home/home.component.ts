84f19160ef33871cdeab427d336dde8d83a7f5e8
anibalfn <anibalneto29@gmail.com>
2024-02-14 04:37:58 -0300

    Change big-card component and title to be mobile responsible

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  public articleTitle: string = "MY ARTICLES"
+  public articleTitle: string = "MAIN PROJECTS"
 }
