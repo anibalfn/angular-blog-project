3e51c4a32b27cccfe2dfbeda360b05786b3c331d
anibalfn <anibalneto29@gmail.com>
2024-02-15 02:22:07 -0300

    Add font to the menu-bar in router, add infos to bio

--- src/app/components/pages/bio/bio.component.ts
+++ src/app/components/pages/bio/bio.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './bio.component.css'
 })
 export class BioComponent {
+photoCover: string = 'https://avatars.githubusercontent.com/u/78622296?v=4';
 
 }
