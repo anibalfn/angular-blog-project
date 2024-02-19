0fa80116c06b9a855b42d7cf4a8d69a439f98036
anibalfn <anibalneto29@gmail.com>
2024-02-19 06:26:39 -0300

    Add components structure in pages/bio

---
+++ src/app/components/pages/bio/findme/findme.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-findme',
+  templateUrl: './findme.component.html',
+  styleUrl: './findme.component.css'
+})
+export class FindmeComponent {
+
+}
